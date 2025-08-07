4f10e18a994e2e2d6cc17303de7906c69c32046a
Aditya Goyal <141710136+AdityaGoyal1505@users.noreply.github.com>
2025-08-07 14:25:14 +0530

    Update ga4.js

--- ga4.js
+++ ga4.js
@@ -4,7 +4,7 @@ const { BetaAnalyticsDataClient } = require('@google-analytics/data');
 const analyticsDataClient = new BetaAnalyticsDataClient({
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.reple(/\\n/g, '\n'),
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   },
 });
 
